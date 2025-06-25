127f58f5bf84f2c8f2c8096fd315e8c0abdac844
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-06-25 14:13:13 +0530

    authentication done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>hii
-      <br />
-       <Button>Button</Button>
+    <div>
+      home
     </div>
   );
 }
