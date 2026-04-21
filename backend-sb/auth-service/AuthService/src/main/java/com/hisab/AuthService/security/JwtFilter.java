c973eb5473b98331db1c6c0203583777fb35ed8f
kaushal-Prakash <kingkaushal1289@gmail.com>
2026-04-22 02:03:04 +0530

    added security package implementing JwtFilter and JwtUtility

---
+++ backend-sb/auth-service/AuthService/src/main/java/com/hisab/AuthService/security/JwtFilter.java
@@ -0,0 +1,47 @@
+package com.hisab.AuthService.security;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class JwtFilter implements Filter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        String authHeader = req.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+
+            try {
+                String userId = jwtUtil.extractUserId(token);
+
+                // Attach userId like req.user
+                req.setAttribute("userId", userId);
+
+            } catch (Exception e) {
+                System.out.println("Invalid JWT");
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+}
---
+++ backend-sb/auth-service/AuthService/src/main/java/com/hisab/AuthService/security/JwtUtil.java
@@ -0,0 +1,36 @@
+package com.hisab.AuthService.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "your-secret-key-your-secret-key"; // keep long
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String userId) {
+        return Jwts.builder()
+                .subject(userId)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUserId(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+}
