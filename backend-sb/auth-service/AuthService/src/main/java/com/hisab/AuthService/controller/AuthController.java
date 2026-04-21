a73b813b91137a69bfc8b619f7f959c670db2033
kaushal-Prakash <kingkaushal1289@gmail.com>
2026-04-22 01:12:40 +0530

    feat: added basic auth backend setup

---
+++ backend-sb/auth-service/AuthService/src/main/java/com/hisab/AuthService/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.hisab.AuthService.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Auth Service!";
+    }
+}
---
+++ backend-sb/auth-service/AuthService/src/test/java/com/hisab/AuthService/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hisab.AuthService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
